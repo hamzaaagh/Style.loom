17e47a0651497c657f5b4c4abce268b1ec79a139
Hamza <ghafarhamzaamsn.777@gmail.com>
2025-10-13 12:11:39 +0300

    create Consts file and create Color Consts file

---
+++ lib/core/Consts/color_consts.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/widgets.dart';
+
+abstract class Consts {
+  // 🎨 Brown palette
+  static const Color brown60 = Color(0xFFAE9B84);
+  static const Color brown65 = Color(0xFFB8A893);
+  static const Color brown70 = Color(0xFFC2B4A3);
+  static const Color brown80 = Color(0xFFD6CDC2);
+  static const Color brown90 = Color(0xFFEBE6E0);
+  static const Color brown95 = Color(0xFFF5F3F0);
+  static const Color brown97 = Color(0xFFF9F8F6);
+  static const Color brown99 = Color(0xFFFDFDFC);
+  //  Black palette
+  static const Color black06 = Color(0xFF0F0F0F);
+  static const Color black10 = Color(0xFF1A1A1A);
+  static const Color black12 = Color(0xFF1F1F1F);
+  static const Color black15 = Color(0xFF262626);
+  static const Color black20 = Color(0xFF333333);
+  static const Color black25 = Color(0xFF404040);
+  static const Color black30 = Color(0xFF4D4D4D);
+  static const Color black35 = Color(0xFF595959);
+
+  // Gray palette
+  static const Color gray40 = Color(0xFF676665);
+  static const Color gray50 = Color(0xFF81807E);
+  static const Color gray70 = Color(0xFFB3B3B2);
+  static const Color gray80 = Color(0xFFCCCCCC);
+  static const Color gray90 = Color(0xFFE6E6E6);
+  static const Color gray95 = Color(0xFFF2F2F2);
+  static const Color gray97 = Color(0xFFF7F7F7);
+  static const Color gray99 = Color(0xFFFCFCFC);
+}
