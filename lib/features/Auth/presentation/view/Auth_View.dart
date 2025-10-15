aac06b8de869811f9952cdf1a5cc58b920d45f3f
Hamza <ghafarhamzaamsn.777@gmail.com>
2025-10-15 20:12:37 +0300

    create Auth View Body

--- lib/features/Auth/presentation/view/Auth_View.dart
+++ lib/features/Auth/presentation/view/Auth_View.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:style/features/Auth/presentation/view/Widgets/Auth_View_Body.dart';
 
 class AuthView extends StatelessWidget {
   const AuthView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(child: Scaffold(body: AuthViewBody()));
   }
 }
---
+++ lib/features/Auth/presentation/view/Widgets/Auth_View_Body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AuthViewBody extends StatelessWidget {
+  const AuthViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
