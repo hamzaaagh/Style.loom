cf0d5d4ec3ea7246391aa2fc2d31f3c8d38c4115
Hamza <ghafarhamzaamsn.777@gmail.com>
2025-11-10 11:05:50 +0300

    create Expansion Tile

---
+++ lib/features/Product_Details/presentation/view/Widgets/Custom_Expansion_Tile.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:style/core/Consts/color_consts.dart';
+
+class CustomExpansionTile extends StatefulWidget {
+  const CustomExpansionTile({
+    super.key,
+    required this.title,
+    required this.longtitle,
+  });
+  final String title;
+  final String longtitle;
+
+  @override
+  State<CustomExpansionTile> createState() => _CustomExpansionTileState();
+}
+
+class _CustomExpansionTileState extends State<CustomExpansionTile>
+    with SingleTickerProviderStateMixin {
+  // ignore: unused_field
+  bool _isExpanded = false;
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTap(bool expanded) {
+    setState(() => _isExpanded = expanded);
+    if (expanded) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ExpansionTile(
+        collapsedShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        expandedAlignment: Alignment.centerLeft,
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.white, fontSize: 23),
+        ),
+        collapsedBackgroundColor: Consts.black25,
+        onExpansionChanged: _handleTap,
+        backgroundColor: Consts.black25,
+        collapsedIconColor: Colors.white,
+        trailing: RotationTransition(
+          turns: Tween(begin: 0.0, end: 0.25).animate(_controller),
+          child: Icon(Icons.arrow_forward_ios, color: Colors.white, size: 16),
+        ),
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              widget.longtitle,
+              style: TextStyle(color: Colors.white, fontSize: 14),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/Product_Details/presentation/view/Widgets/Product_details_view_body.dart
+++ lib/features/Product_Details/presentation/view/Widgets/Product_details_view_body.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:style/core/Consts/color_consts.dart';
-import 'package:style/core/Widgets/Custom_Bottom.dart';
 
 class ProductDetailsViewBody extends StatelessWidget {
   const ProductDetailsViewBody({super.key});
