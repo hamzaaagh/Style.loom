a1fd90df849a5bcae5acd45699160ef47ec9e05a
Hamza <ghafarhamzaamsn.777@gmail.com>
2025-11-13 15:27:12 +0300

    create similiar items views

---
+++ lib/features/Product_Details/presentation/manager/Fetch_Similiar_Item_Cubit/fetch_similiar_item_state.dart
@@ -0,0 +1,24 @@
+// part of 'fetch_similiar_item_cubit.dart';
+
+// sealed class FetchSimiliarItemState extends Equatable {
+//   const FetchSimiliarItemState();
+
+//   @override
+//   List<Object> get props => [];
+// }
+
+// final class FetchSimiliarItemInitial extends FetchSimiliarItemState {}
+
+// final class FetchSimiliarItemLoading extends FetchSimiliarItemState {}
+
+// final class FetchSimiliarItemSucces extends FetchSimiliarItemState {
+//   final List<ProductModel> similiarproducts;
+
+//   const FetchSimiliarItemSucces({required this.similiarproducts});
+// }
+
+// final class FetchSimiliarItemFailure extends FetchSimiliarItemState {
+//   final String errmessage;
+
+//   const FetchSimiliarItemFailure({required this.errmessage});
+// }
